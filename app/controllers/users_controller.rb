a3a52bce75532fe9ac2d367db596b0b0ad750ea2
hiroki-norota <cake.fujiwara@gmail.com>
2020-09-05 23:46:52 +0900

    user_controllerのupdate method記述

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,23 @@ class UsersController < ApplicationController
   def show
   end
 
+  def update
+    if current_user.update(user_params)
+      render_to root_path
+    else
+      render :edit
+    end
+  end
+
   def logout
   end
 
   def edit
   end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
