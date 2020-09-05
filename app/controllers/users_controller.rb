afc8ffabdf1e15c70a5d54bb4be89eed4ec8f14c
hiroki-norota <cake.fujiwara@gmail.com>
2020-09-06 00:20:13 +0900

    user_controller不要なmethod delete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,18 +3,12 @@ class UsersController < ApplicationController
   def index
   end
 
-  def show
-  end
-
   def update
     if current_user.update(user_params)
       render_to root_path
     else
       render :edit
     end
-  end
-
-  def logout
   end
 
   def edit
